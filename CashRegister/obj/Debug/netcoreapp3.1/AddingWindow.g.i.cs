c31906a812308fec714e2ec18b3acf75ce6d4005
mastersalem <mikolaj.sumyk.art@gmail.com>
2021-05-20 18:22:58 +0200

    Main UI done

--- CashRegister/obj/Debug/netcoreapp3.1/AddingWindow.g.i.cs
+++ CashRegister/obj/Debug/netcoreapp3.1/AddingWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace CashRegister {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CashRegister;V1.0.0.0;component/addingwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CashRegister;component/addingwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AddingWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
